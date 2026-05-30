cca2e194017f2669576276b12efd1cf8447ebd27
Lars (LD) <ldaneliee33@gmail.com>
2026-05-30 18:08:24 +0800

    Promote timajo_3.py from UAT to Production

---
+++ Production/timajo_3.py
@@ -0,0 +1,14 @@
+def main():
+    for i in range(1, 21):
+        print(i, end=" ")
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print("Invalid.")
+
+if __name__ == "__main__":
+    main()
