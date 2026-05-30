f7c4b704997fbbe764e58c8fd9f577b75348fa13
vectorheart <136724513+vectorheart@users.noreply.github.com>
2026-05-30 17:34:57 +0800

    Deploy josh_2.py to Dev

---
+++ Dev/josh_2.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    return a + b
+    pass
+
+print(add(3, 5))
+
+def subtract(a, b):
+    return a - b
+    pass
+
+print(subtract(3, 5))
+
+def multiply(a, b):
+    return a * b 
+    pass
+
+print(multiply(3, 5))
+
+def divide(a, b):
+    if b == 0:
+        return "cannot divide by zero"
+
+    return a / b
+    pass
+
+print(divide(3, 5))
