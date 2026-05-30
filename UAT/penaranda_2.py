4fd67958e2a14d73865101f53a8e5d8444a54d60
Noah Peñaranda <143115018+nonoro-official@users.noreply.github.com>
2026-05-30 18:02:23 +0800

    Promote penaranda_2.py from Dev to UAT

---
+++ UAT/penaranda_2.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Cannot divide by zero."
+    return a / b
+
+print(add(9, 3))
+print(sub(9, 3))
+print(mul(9, 3))
+print(div(9, 3))
