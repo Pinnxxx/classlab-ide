fbf68d4ac422eb8b6a6b181d24ade0e3b7a72b08
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2026-05-30 17:55:33 +0800

    Promote salonga-2.py from Dev to UAT

--- UAT/salonga-2.py
+++ UAT/salonga-2.py
@@ -1,5 +1,16 @@
 import random 
 
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
 def div(a, b):
     if b == 0:
         return "cannot divide by zero"
@@ -8,4 +19,8 @@ def div(a, b):
 
 num1 = random.randint(1,5)
 num2 = random.randint(0,6)
+
+print( str(num1) + "+" + str(num2) + " = " + str(add(num1,num2)))
+print( str(num1) + "-" + str(num2) + " = " + str(sub(num1,num2)))
+print( str(num1) + "*" + str(num2) + " = " + str(multiply(num1,num2)))
 print( str(num1) + "/" + str(num2) + " = " + str(div(num1,num2)))
