6b9fa3912ff1b505115db8952dcab90c70079d27
NormalGuy866 <eimercanapi22@gmail.com>
2026-05-30 18:08:37 +0800

    Promote canapilab5.py from Dev to UAT

---
+++ UAT/canapilab5.py
@@ -0,0 +1,7 @@
+def is_palindrome(text):
+    s = text.lower().replace(" ", "")
+    return s == s[::-1]
+
+print(is_palindrome("racecar"))
+print(is_palindrome("hello"))
+print(is_palindrome("Was it a car or a cat I saw"))
