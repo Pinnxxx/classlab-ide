3cb2eef0f98bdf65813ce1bf1d4f2cab91d9a10c
Jeff Marquez <jeffmarquez2006@gmail.com>
2026-05-30 20:12:52 +0800

    Promote Marquez_3.py from UAT to Production

---
+++ Production/Marquez_3.py
@@ -0,0 +1,8 @@
+def print_times_table(n):
+    # Loop for 1-10 times
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+
+# Set the target number
+target_number = 67
+print_times_table(target_number)
