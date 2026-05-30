6ecb5a417906ec1d16f347a88b0075188ae68edb
whyburrito <152293116+whyburrito@users.noreply.github.com>
2026-05-30 20:01:00 +0800

    Deploy salvador-4.py to Dev

---
+++ Dev/salvador-4.py
@@ -0,0 +1,16 @@
+import unittest
+
+def largest(a, b, c):
+    """Returns the largest of three numbers."""
+    return max(a, b, c)
+
+class T(unittest.TestCase):
+    def test_largest(self):
+        self.assertEqual(largest(4, 9, 2), 9)
+        self.assertEqual(largest(10, 5, 8), 10) 
+        self.assertEqual(largest(3, 3, 1), 3)    
+        self.assertEqual(largest(-5, -2, -10), -2) 
+
+if __name__ == '__main__':
+    # Tell unittest not to shut down the environment
+    unittest.main(exit=False)
