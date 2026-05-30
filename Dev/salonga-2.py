80a29f60a2311f9b5ced9bf702f2fe2fbac6c5b1
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2026-05-30 17:43:14 +0800

    Deploy salonga-2.py to Dev

---
+++ Dev/salonga-2.py
@@ -0,0 +1,11 @@
+import random 
+
+def div(a, b):
+    if b == 0:
+        return "cannot divide by zero"
+    return a / b
+
+
+num1 = random.randint(1,5)
+num2 = random.randint(0,6)
+print( str(num1) + "/" + str(num2) + " = " + str(div(num1,num2)))
